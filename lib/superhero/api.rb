55e8c7cbb03c23a00d50b99e1d1f9f8ae553ff34
John Carter <johncarter352@gmail.com>
2020-05-16 21:36:41 +0000

    updated class api

--- lib/superhero/api.rb
+++ lib/superhero/api.rb
@@ -1,9 +1,9 @@
 class API
-  def self.get_hero(hero)
+  def self.get_hero(heroname)
     ## what is my endpoint
-    
+    url = "https://superheroapi.com/api/access-token/search/name"
     ### how do I go there and how do I get what I need (will return json)
-    
+    response = Net::HTTP.get(URI(url))
     # how do I handle that json and turn it into meaningful data?
     
     ## how do IU make drink objects from that data 

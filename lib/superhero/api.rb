9fa8861f0e1edb23212cc2e1780b18647b3f90e6
John Carter <johncarter352@gmail.com>
2020-05-17 22:23:11 +0000

    update

--- lib/superhero/api.rb
+++ lib/superhero/api.rb
@@ -4,45 +4,27 @@ class API
   def self.get_hero(name)
     url = "https://www.superheroapi.com/api.php/10213815254774657/search/#{name}"
     response = Net::HTTP.get(URI(url))
-    @object = JSON.parse(response, object_class: OpenStruct)
+    object = JSON.parse(response)
+    results = object["results"]
      
     
   end
   
-  def self.result
-    @object.results
-   
-  end
-  
   def self.get_bio
-    bio = @object.results[0].biography
-    bio.each do |a|
-      
-    end
-    bio
+   results[0]["biography"]
   end 
   
   def self.get_stats
-    ps = @object.results[0].powerstats
-    ps.each do |a|
-      puts a 
-    end
-    ps
+  results[0]["powerstats"]
   end 
   
   def self.get_appearance
-    app = @object.results[0].appearance
-    app.each do |a|
-      puts a 
-    end
+    results[0]["appearance"]
   end 
   
   def self.get_work
-   work = @object.results[0].work
-   work.each do |a|
-     puts a 
-   end
- end
+    results[0]["work"]
+
   
 end
 

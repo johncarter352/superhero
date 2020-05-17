c637b14773483a91f68db1d24c13b8fef9353902
John Carter <johncarter352@gmail.com>
2020-05-17 00:49:29 +0000

    progress

--- lib/superhero/api.rb
+++ lib/superhero/api.rb
@@ -1,16 +1,13 @@
 class API
   def self.get_hero(name)
-    ## what is my endpoint
     url = "https://www.superheroapi.com/api.php/10213815254774657/search/#{name}"
-    ### how do I go there and how do I get what I need (will return json)
+   
     response = Net::HTTP.get(URI(url))
   
-    # how do I handle that json and turn it into meaningful data?
-     #results = JSON.parse(response)["results"]
-     objects = JSON.parse(response, object_class: OpenStruct)
+   
+     object = JSON.parse(response, object_class: OpenStruct)
+     object.response[0]
      
-    
-    ## how do IU make drink objects from that data 
      binding.pry
   end
   
